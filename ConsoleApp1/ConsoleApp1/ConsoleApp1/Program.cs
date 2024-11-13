5c195a218154c8910058fc1e0b1bcc9150088cb7
Alucard504 <febrianorahadian88@gmail.com>
2024-11-13 07:30:20 +0700

    init

---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.AccessControl;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Rumus Matematika");
+        Console.WriteLine("1.Rumus Segitiga");
+        Console.WriteLine("2.Rumus Jajargenjang");
+        Console.Write("Pilih mode: ");
+        int mode = Convert.ToInt32(Console.ReadLine());
+
+        if (mode == 1) {
+            op_luas_segitiga();
+        } else if (mode == 2) {
+            op_Luas_jajargenjang();
+        } else {
+
+        }
+    }
+
+    static void op_luas_segitiga() {
+
+        //minta pengguna memasukan nilai alas
+        Console.Write("Masukan alas segitiga: ");
+        double alas = Convert.ToDouble(Console.ReadLine());
+        //minta pengguna memasukan nilai tinggi
+        Console.Write("Masukan tinggi segitiga: ");
+        double tinggi = Convert.ToDouble(Console.ReadLine());
+        //Hitung luas segitiga
+        double luas = 0.5 * alas * tinggi;
+
+        Console.WriteLine("Luas segitiga adalah: "+ luas);
+    }
+
+    static void op_Luas_jajargenjang() {
+
+        //minta pengguna memasukan nilai alas 
+        Console.WriteLine("Masukan alas jajargenjang");
+        double alas = Convert.ToDouble(Console.ReadLine());
+        //minta pengguna memasukan nilai tinggi
+        Console.WriteLine("Masukan tinggi jajargenjang");
+        double tinggi = Convert.ToDouble(Console.ReadLine());
+        //menghitung luas jajargenjang
+        double luas = alas * tinggi;
+
+        Console.WriteLine("Luas jajargenjang adalah: " + luas);
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("==========================");
+        Console.WriteLine(" Menghitung Luas Segitiga ");
+        Console.WriteLine("==========================");
+        //Minta pengguna memasukan nilai alas
+        Console.Write("Nilai Alas Segitiga: ");
+        double Alas = Convert.ToDouble(Console.ReadLine());
+
+        //Minta pengguna memasukan nilai tinggi
+        Console.WriteLine("Nilai Tinggi Segitiga");
+        double Tinggi = Convert.ToDouble(Console.ReadLine());
+        
+        double Luas = 0.5 * Alas * Tinggi;
+
+        Console.WriteLine("luas Segitiga adalah: " + Luas);
+    }
+}
